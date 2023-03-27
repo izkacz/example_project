18bc990007d694c7b89e5320540b1ee36db9f1dc
Iza <izaczum@gmail.com>
2023-03-27 12:17:58 +0200

    pole i obwod prostokata

--- ex_2.py
+++ ex_2.py
@@ -3,5 +3,12 @@ a=10
 obwod=a*4
 pole = a*a
 
-
 print("Obwód kwadratu wynosi"+str(obwod)+",a pole" +str(pole)+ ".")
+
+#prostokąt
+
+b=5
+obwodProst=(a*2)*(b*2)
+poleProst=a*b
+
+print("Obwód prostokąta wynosi"+str(obwodProst)+",a pole" +str(poleProst)+ ".")
