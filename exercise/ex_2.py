1f05aee745f064f9b5740ab0678655b62cc5e104
Iza <izaczum@gmail.com>
2023-03-27 17:25:19 +0200

    made ex_2 more pretty because I fixed my ide

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -1,9 +1,11 @@
+
+import math
 #kwadrat
 a=10
 obwod=a*4
 pole = a*a
 
-print("Obwód kwadratu wynosi"+str(obwod)+",a pole" +str(pole)+ ".")
+print("Obwód kwadratu wynosi "+str(obwod)+", a pole " +str(pole)+ ".")
 
 #prostokąt
 
@@ -11,23 +13,23 @@ b=5
 obwodProst=(a*2)*(b*2)
 poleProst=a*b
 
-print("Obwód prostokąta wynosi"+str(obwodProst)+",a pole" +str(poleProst)+ ".")
+print("Obwód prostokąta wynosi "+str(obwodProst)+", a pole " +str(poleProst)+ ".")
 
 #trapez
 h=2
 r=5
 obwodTrap=a+b+(2*r)
 poleTrap=((a+b)*h/2)
-print("Obwód trapezu wynosi"+str(obwodTrap)+",a pole" +str(poleTrap)+ ".")
+print("Obwód trapezu wynosi "+str(obwodTrap)+", a pole " +str(poleTrap)+ ".")
 
 #koło
-pi_zmienna=math.pi
+pi_zmienna= math.pi
 obwodKol=2*pi_zmienna*r
 poleKol=pi_zmienna*r**2
-print("Obwód koła wynosi"+str(obwodKol)+",a pole" +str(poleKol)+ ".")
+print("Obwód koła wynosi "+str(obwodKol)+", a pole " +str(poleKol)+ ".")
 
 #trojkat
 c=7
 obwodTroj=a+b+c
 poleTroj=(a+b)*h/2
-print("Obwód trójkąta wynosi"+str(obwodTroj)+",a pole" +str(poleTroj)+ ".")
+print("Obwód trójkąta wynosi "+str(obwodTroj)+", a pole " +str(poleTroj)+ ".")
