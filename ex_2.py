096a38e3b60ad313dd4f3ee191e9c83860e148c9
Iza <izaczum@gmail.com>
2023-03-27 12:24:09 +0200

    pole i obwod trapezu

--- ex_2.py
+++ ex_2.py
@@ -12,3 +12,10 @@ obwodProst=(a*2)*(b*2)
 poleProst=a*b
 
 print("Obwód prostokąta wynosi"+str(obwodProst)+",a pole" +str(poleProst)+ ".")
+
+#trapez
+h=2
+r=5
+obwodTrap=a+b+(2*r)
+poleTrap=((a+b)*h/2)
+print("Obwód trapezu wynosi"+str(obwodTrap)+",a pole" +str(poleTrap)+ ".")
