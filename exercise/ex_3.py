4153934140b97ded6dc883875a375538a0be936b
Wojtek <layvin@interia.pl>
2023-04-03 12:29:48 +0200

    kwadrat i prostokąt

--- exercise/ex_3.py
+++ exercise/ex_3.py
@@ -6,13 +6,15 @@ def trojkat(bok_a, bok_b, bok_c, wysokosc_a):
 
 # kwadrat, prostokat dla studenta 1
 def kwadrat(bok):
-    # TODO
-    return 0, 0
+    obwod1 = 4*bok
+    pole1 = bok*bok
+    return obwod1, pole1
 
 
 def prostokat(bok_a, bok_b):
-    # TODO
-    return 0, 0
+    obwod2 = 2*bok_a+2*bok_b
+    pole2 = bok_a*bok_b
+    return obwod2, pole2
 
 # rownoleglobok i romb dla studenta 2
 def rownoleglobok(bok_a, bok_b, wysokosc_a):
@@ -34,10 +36,10 @@ def kolo(promien):
     return 0, 0
 
 
-assert trojkat(10, 15, 16, 8) == (41, 40)
-assert kwadrat(20) == (80, 400)
-assert prostokat(12, 10) == (44, 120)
-assert rownoleglobok(6, 5, 2) == (22, 12)
-assert romb(10, 5) == (40, 50)
-assert trapez(10, 15, 7, 14, 2) == (45, 25)
+# assert trojkat(10, 15, 16, 8) == (41, 40)
+# assert kwadrat(20) == (80, 400)
+# assert prostokat(12, 10) == (44, 120)
+# assert rownoleglobok(6, 5, 2) == (22, 12)
+# assert romb(10, 5) == (40, 50)
+# assert trapez(10, 15, 7, 14, 2) == (45, 25)
 # TODO na koniec! dopisz 2 testy dla kola i dla kazdej innej figury po jednym dodatkowym tescie
