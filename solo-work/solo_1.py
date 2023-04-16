5438985c44ea0187a5ed9b8ce48d792d51ba267c
jan <jan.galazka2108@gmail.com>
2023-04-16 23:17:29 +0200

    zadanie 1.3

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -6,3 +6,8 @@ print("{} {}".format(hello, student))
 # zadanie 1.2
 student = input("Wpisz swoje imie")
 print("Hello", student)
+
+# zadanie 1.3
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
+liczba_studentow = len(studenci)
+print("Liczba studentow wynosi:", liczba_studentow)
