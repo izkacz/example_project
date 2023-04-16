a9f50fe8459b93e9526362a1e3c02e46ac78ffab
jan <jan.galazka2108@gmail.com>
2023-04-16 23:39:59 +0200

    zadanie 1.8

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -37,3 +37,17 @@ studenci.sort()
 print("Alfabetyczna lista studentow wynosi: ")
 for student in studenci:
     print(student)
+
+# zadanie 1.8
+
+# posortuj alfabetycznie (od nazwiska) studentow
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+studenci_posortowani = sorted(studenci, key=lambda x: x.split()[-1])
+# oczekiwany rezultat:
+# Barbara Kowalska
+# Jan Niezbedny
+# Tomasz Nijaki
+# Anna Szczesny
+print("Alfabetyczna lista studentow wynosi: ")
+for student in studenci_posortowani:
+    print(student)
